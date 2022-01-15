e6f04f0123b8acbe4ae443f80982802ce179c07b
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2022-01-15 14:34:29 -0500

    Addingnavbar

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
