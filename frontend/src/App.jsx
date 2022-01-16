1ba9f8f25e32d23bf9edd51d8d2b6ec576246565
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2022-01-16 16:28:19 -0500

    Added list for stocks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/navbar";
+import VirtualizedList from "./components/list";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <h1>Tradeable Assets</h1>
+      <VirtualizedList />
     </div>
   );
 }
