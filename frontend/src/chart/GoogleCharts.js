81f573c6c4cfb8cf844c7f9cb44480e8538a723f
Awab Osman <awabosman3@gmail.com>
2022-01-21 01:39:55 -0500

    login logout fixes

--- frontend/src/chart/GoogleCharts.js
+++ frontend/src/chart/GoogleCharts.js
@@ -1,48 +1,50 @@
 import React, { useState, useEffect } from "react";
 import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
-// const { google_api } = require ("../../googleTrends")
+import axios from "axios"
 
-// const GoogleCharts = () => {
-//   const [chart, setChart] = useState({ labels: [], data: [] });
+const GoogleCharts = () => {
+  const [chart, setChart] = useState({ labels: [], data: [] });
 
-//   useEffect(() => {
-//     google_api().then((res) => {
-//       setChart({ labels: res[0], data: res[1] });
-//     });
-//   }, []);
+  useEffect(() => {
+    axios.get("http://localhost:3001/api/trends")
+    .then((res) => {
+      console.log(res.data)
+      setChart({ labels: res.data[0], data: res.data[1] });
+    });
+  }, []);
 
-//   useEffect(() => {
-//     const data = {
-//       labels: chart.labels,
-//       datasets: [
-//         {
-//           label: `Search history`,
-//           backgroundColor: "rgb(255, 99, 132)",
-//           borderColor: "rgb(255, 99, 132)",
-//           data: chart.data,
-//         },
-//       ],
-//     };
+  useEffect(() => {
+    const data = {
+      labels: chart.labels,
+      datasets: [
+        {
+          label: `Search history`,
+          backgroundColor: "rgb(47, 247, 237)",
+          borderColor: "rgb(255, 99, 132)",
+          data: chart.data,
+        },
+      ],
+    };
 
-//     const config = {
-//       type: "line",
-//       data: data,
-//       options: {},
-//     };
+    const config = {
+      type: "line",
+      data: data,
+      options: {},
+    };
 
-//     const myChart = new Chart(document.getElementById("myChart"), config);
+    const myChart = new Chart(document.getElementById("myChart"), config);
 
-//     return function destory() {
-//       myChart.destroy();
-//     };
-//   }, [chart]);
+    return function destory() {
+      myChart.destroy();
+    };
+  }, [chart]);
 
-//   return (
-//     <div>
-//       <canvas id="myChart"></canvas>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <canvas id="myChart"></canvas>
+    </div>
+  );
+};
 
-// export default GoogleCharts;
+export default GoogleCharts;
--- frontend/src/routes/Favorites.jsx
+++ frontend/src/routes/Favorites.jsx
@@ -6,7 +6,7 @@ export default function Favorties() {
     <div className="fav">
       <NavBar />
       <h1>Hello people</h1>
-
+      <GoogleCharts />
     </div>
   );
 }
