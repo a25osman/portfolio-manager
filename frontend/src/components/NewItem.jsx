4773e3dcfd7c27654d79bbcc9ef633e712158c60
amoham1 <amoham456@gmail.com>
2022-01-23 17:56:51 -0500

    css for the news page

--- frontend/src/components/NewItem.jsx
+++ frontend/src/components/NewItem.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
+import '../css/Newscss.css'
 
 export const NewItem = ({urlToImage, url, description, title}) => {
   return (
-    <div>
-      <img src={urlToImage} alt="New Image" />
-      <h3>
+    <div className="news-section">
+      <img className="news-image" src={urlToImage} alt="New Image" />
+      <h3 className="news_text">
         <a href={url}>{title}</a>
-        <p>{description}</p>
+        <p className="news_description">{description}</p>
       </h3>
     </div>
   )
 }
 
 export default NewItem;
+
 
