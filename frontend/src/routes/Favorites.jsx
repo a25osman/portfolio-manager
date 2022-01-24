ac4915130ef243064ba00cf5f589ac1c2d5cba8e
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2022-01-23 22:06:11 -0500

    Favorites heading

--- frontend/src/routes/Favorites.jsx
+++ frontend/src/routes/Favorites.jsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import GoogleCharts from "../chart/GoogleCharts";
 import NavBar from "../components/NavBar";
-import PortfolioCharts from "../chart/PortfolioCharts"
+import PortfolioCharts from "../chart/PortfolioCharts";
 export default function Favorties() {
   return (
     <div className="fav">
       <NavBar />
-      <h1>Hello people</h1>
-      <GoogleCharts /> 
+      <h1>Google Search Trends</h1>
+      <GoogleCharts />
     </div>
   );
 }
