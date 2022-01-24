132a917dc6f8b22587eae33b9d4a01ff9a514d5f
Isaiah Mutekanga <isaiahmutekanga@trentu.ca>
2022-01-24 14:43:34 -0500

    Added delete button

--- frontend/src/components/UserTransactions.jsx
+++ frontend/src/components/UserTransactions.jsx
@@ -1,24 +1,26 @@
 import { Fragment, useState } from "react";
 import { useEffect } from "react";
 import axios from "axios";
-
+import Button from "@mui/material/Button";
 
 const UserTransactions = () => {
   const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
     axios
-      .get(
-        `http://localhost:3001/api/transactions/dsanders/1`
-      )
+      .get(`http://localhost:3001/api/transactions/dsanders/1`)
       .then((res) => {
-        setTransactions(res.data)
+        setTransactions(res.data);
       });
   }, []);
 
   const transactionDelete = () => {
-
-  }
+    axios
+      .post(`http://localhost:3001/api/transactions/1/delete`)
+      .then((res) => {
+        console.log(res.data);
+      });
+  };
 
   return (
     <div className="Table">
@@ -34,40 +36,42 @@ const UserTransactions = () => {
             <th>Delete Button:</th>
           </tr>
         </thead>
-        {
-          transactions.map((transaction) => {
-            return (
-              <>
-                <tbody>
-                  <tr key={transaction.id}>
-                    <td>{transaction.coin_name}</td>
-                    <td>{transaction.coin_symbol}</td>
-                    <td>{transaction.quantity}</td>
-                    <td>{transaction.exchange_symbol}</td>
-                    <td>{transaction.exchange_value}</td>
-                    <td>{transaction.date}</td>
-                    <td>{/* <button onClick={transactionDelete(transaction.id)}>Delete Button</button> */}</td>
-                  </tr>
-                </tbody>
+        {transactions.map((transaction) => {
+          return (
+            <>
+              <tbody>
+                <tr key={transaction.id}>
+                  <td>{transaction.coin_name}</td>
+                  <td>{transaction.coin_symbol}</td>
+                  <td>{transaction.quantity}</td>
+                  <td>{transaction.exchange_symbol}</td>
+                  <td>{transaction.exchange_value}</td>
+                  <td>{transaction.date}</td>
+                  <td>
+                    <Button
+                      type="submit"
+                      onClick={transactionDelete}
+                      variant="contained"
+                    >
+                      Delete
+                    </Button>
+                  </td>
+                </tr>
+              </tbody>
 
-
-                {/* <p>Coins Name: {transaction.coin_name}</p>
+              {/* <p>Coins Name: {transaction.coin_name}</p>
                   <p>Coins Symbol: {transaction.coin_symbol}</p>
                   <p>Amount in Own: {transaction.quantity}</p>
                   <p>Currency: {transaction.exchange_symbol}</p>
                   <p>Coins value: {transaction.exchange_value}</p>
                   <p>Date Bought: {transaction.date} </p>
                   <button onClick={transactionDelete(transaction.id)}>Delete Button</button> */}
-
-              </>
-            )
-          })
-
-        }
+            </>
+          );
+        })}
       </table>
     </div>
   );
-
-}
+};
 
 export default UserTransactions;
--- frontend/src/routes/Portfolio.jsx
+++ frontend/src/routes/Portfolio.jsx
@@ -37,7 +37,7 @@ const Portfolio = (props) => {
         }
       });
     });
-  console.log(currentUser);
+
   return (
     <div>
       <NavBar currentUser={user} />
